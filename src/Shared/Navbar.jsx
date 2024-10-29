721b5a24098b7b8aed9559b90f476e6afc07c2bf
Akm Nahid Bin Wadood <nahidbinwadood@gmail.com>
2024-10-29 10:07:26 +0600

    updated images

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -3,9 +3,11 @@ import logoWhite from '../assets/images/logo.svg';
 import logoBlack from '../assets/images/logo-black.png';
 
 import { CartSvg, ProfileSvg, SearchSvg } from './../Components/SvgContainer';
+import { useState } from 'react';
 
 const Navbar = () => {
   const isHomepage = useLocation().pathname;
+  const [showLogin, setShowLogin] = useState(false);
 
   return (
     <div className="absolute h-[88px] w-full bg-transparent top-0 left-0">
@@ -24,14 +26,27 @@ const Navbar = () => {
           <NavLink to="/shop">Shop</NavLink>
         </div>
         <div className="flex items-center gap-6">
-          <div>
+          <div className="cursor-pointer">
             {isHomepage == '/' ? <SearchSvg /> : <SearchSvg light={true} />}
           </div>
-          <div>
+          <Link to='/dashboard/home' className="cursor-pointer">
             {isHomepage == '/' ? <CartSvg /> : <CartSvg light={true} />}
-          </div>
-          <div>
+          </Link>
+          <div
+            onClick={() => setShowLogin(!showLogin)}
+            className="cursor-pointer relative"
+          >
             {isHomepage == '/' ? <ProfileSvg /> : <ProfileSvg light={true} />}
+            {showLogin ? (
+              <div className="bg-white px-6 py-4 font-medium rounded-md shadow-lg absolute  mt-2 flex flex-col justify-center text-center">
+                <Link to='/login' className="text-sm pb-2 border-b border-black/50">
+                  Login
+                </Link>
+                <Link to='/shop' className="text-sm pt-2">Purchase</Link>
+              </div>
+            ) : (
+              ''
+            )}
           </div>
         </div>
       </div>
