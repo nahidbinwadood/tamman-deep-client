31ca4f652654b5850c7c266f7c331b4d027b656e
Akm Nahid Bin Wadood <nahidbinwadood@gmail.com>
2024-10-29 19:04:59 +0600

    updated image

--- src/Pages/Homepage/Sections/Growth.jsx
+++ src/Pages/Homepage/Sections/Growth.jsx
@@ -11,9 +11,7 @@ const Growth = () => {
         description={
           'Streamline networking and brand management on-the-go with Quick Link. Manage contacts, schedule meetings, and showcase your products anytime, anywhere.'
         }
-        image={
-          'https://s3-alpha-sig.figma.com/img/f5ac/4fb8/a6d883d29bc37fab002d965ed86f0319?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HS6-SjlU1R9OPJ15VIBhbREBZXcqHrh4I8e4U-sl8BzA7rpTIYlb6lNJ5aJAqSlDRGgZzNU7-KIZVfeLWRKgEJNghmSIDUS6rCvxAYvpvUNKmDmjpM48zj0c~YaefLuTvXFVO0nCeBiJI1tQ7lac73WBf4DQce2DPdlPXSqSisS9K4vTF7fr04uSe1D2y-nvQGw6acZs2gEVTq96-AxeIZa3UQWjyiWf4tZe7sDjCoNutoxrR6p8Mp4PbjUCkWqclHCuiNFd8~enx7i65kjsyH2MdS95zktqGFSzwF0pHFHkbYBjKRZbSQZTPajFzr0K3QOxrK39wCekAE1bFXM5xQ__'
-        }
+        image={'https://i.postimg.cc/qBYxTWB2/design-1.png'}
       />
       <TextImageSection
         title={'Built to '}
@@ -22,9 +20,7 @@ const Growth = () => {
           'Cras porta, turpis quis pulvinar viverra, orci risus consequat velit, sit amet euismod lacus elit in urna.'
         }
         reverse={true}
-        image={
-          'https://s3-alpha-sig.figma.com/img/8286/1056/1c4f72d30364b6b535b1f705af5fca05?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=awwa5iWKt8n4~1NTcV28R-BB5Qcb~e9qc5m5YOwrEYyMT6U6yUe7YnZ9uiFPpMWY7lcLdz7Ac7SXeXXdo0A0esJqcsg49EEe5tj3J3P1teps8JMPm34nMEJkXTdNVr2sFWFXUv5zc20Fzw0UBM~DcMtLP-9JcQuBSOKwN-G3XFbivE5oUKQQwq6BT7VSN7FA9KnLpWlAe2rnAGxBdqL6jTEazfmQyYziQunqXTU6Q50EZoI1lVCkKZa9Ia5w3zxnGJYaQs5wNPamPP7uslfkVOLPa0ix396i3eV3WRaE-lLfKpIl~aaNhPXlVRBlU~qj8RU5A7gvzUMks4Xyd2aeeQ__'
-        }
+        image={'https://i.postimg.cc/Xn5QVNCq/design-2.png'}
       />
     </div>
   );
--- src/Pages/Homepage/Sections/HowItWorks.jsx
+++ src/Pages/Homepage/Sections/HowItWorks.jsx
@@ -4,24 +4,21 @@ import HowItWorksCard from './../../../Components/Cards/HowItWorksCard';
 const HowItWorks = () => {
   const cardsInfo = [
     {
-      image:
-        'https://s3-alpha-sig.figma.com/img/7dbd/56d5/590ee7f9bb8f9343d6dae7ed945303db?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KJG68XGzW3YB7goD5Ca1IpZ5SMepuq53Vso2gGfa7cB88DcoAbmUk160UAyBhJiUoDo9fp1AglFs3BXAxch8SVBWdjGbedKduGd5JLxVnHfmwR2KR5b9S1MePKLzjPNN1qA63P3XHUqjBlrbst-XC8hd73UAWOQY76XNkzJp6B9HsNvT9QTFu3-J~DsWbnkHd5-2afgxvEpZuwQB~rsS1SyaV33UtZpa4sfXQBQSKmQOzPDCHRxqXOKbRXVGMNzcyV52Ttrh0ytIn45C9OKv0yhtPzMR9Cf3vhhBLPyCtgX4tV9~P8OaJWniA9YeKt4MYRboa5zny33C1WWxNX80UA__',
+      image: 'https://i.postimg.cc/j51PRJTR/card-1.png',
       highlightedTitle: 'Step 1',
       title: 'Grab a product',
       description:
         'Forget about Google doc or typing down phone number manually. All it takes is a tap or scan.',
     },
     {
-      image:
-        'https://s3-alpha-sig.figma.com/img/87b7/e868/70b68ddd05aaa9630ff8051385337872?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=V29Z9jgearn4lz-06KCZ67eajD5B~L7KZiVoV~pAFG3FWJN~QomD66k9dQSIX8OTbAB4H1BYnNr6K8xn29no8JbWiLLTW5JW5beJk9c2sUIYAXMrqovQ3xM4Dl4aWzP6lUy-UrGGY1v7RWDI~NcH6eZfrRT6Q7ONjYRiVflQs6sDOJhR1G0RB0jyOUgGa91sMClNsyAsCZB39n9KEc7Uetm6yMR03aNmZuSOpNLNQqMTXBCD1yZCcF~RMUFEDxVJvVwkHrBfyq12c8PU7-ScDaIRZXLeOndltCUQ6BeJ9QxyrZyc-HT0z0q2jkSSFf3g2ZzYyY~yPuoxynRh-XWt9Q__',
+      image: 'https://i.postimg.cc/fy0XyTWm/card-2.png',
       highlightedTitle: 'Step 1',
       title: 'Grab a product',
       description:
         'Others don’t need a QL device or the APP to receive your info. Your profile opens in browser.',
     },
     {
-      image:
-        'https://s3-alpha-sig.figma.com/img/fd0f/fe14/d59b961682cadcb1d858785f6bcc5405?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ba9MyuE3tpoP6L1cw4lp5g2tnem1XduP5W8Rlyema19BEipz6iGcIvDkC2w5Dy4QHY8A0bbc0yKIW~5fUD9OdVx5rPAmd5zQ8hP1h4FmAsEhBgxQV9s5fOMZxsd1Zlo8~QhQRopNGsD~jxkaN3MQ8l7F56cndL0~wKgvYZChxZ-6qcK8eUxSswLSQ37ss8BGZsj0l5qMfxKZPWELts0KBx9-cM4gA2gDY4wolhTd1WF4kyseiWO65AQcOjrtGMx3WpDYuIduqVqB-ZD~sOW5Tot89WkNqkgC0qrAu1OOqU086aFwqR4ZrqNUk172hv2cFHIB~PDK~hyUiI3OckLRcA__',
+      image: 'https://i.postimg.cc/NMFRhkWN/card-3.png',
       highlightedTitle: 'Step 1',
       title: 'Grab a product',
       description:
