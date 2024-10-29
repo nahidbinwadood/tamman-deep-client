5f7113ba9a76a6539ecace087ca55d868442ea45
Akm Nahid Bin Wadood <nahidbinwadood@gmail.com>
2024-10-29 14:30:01 +0600

    updated dashboard shop btn

--- src/Components/Buttons/PrimaryButton.jsx
+++ src/Components/Buttons/PrimaryButton.jsx
@@ -1,11 +1,16 @@
-
 // eslint-disable-next-line react/prop-types
-const PrimaryButton = ({title,fullWidth}) => {
-    return (
-        <button className={`bg-primaryColor px-12 font-semibold text-lg py-4 rounded-lg text-white hover:text-zinc-800  hover:bg-transparent border border-primaryColor transition duration-300 ${fullWidth ? "w-full" :"w-fit"}`}>
-            {title}
-        </button>
-    );
+const PrimaryButton = ({ title, fullWidth, dashboardBtn }) => {
+  return (
+    <button
+      className={`bg-primaryColor px-12 font-semibold text-lg py-4 rounded-lg text-white ${
+        dashboardBtn ? 'hover:text-primaryColor' : 'hover:text-white'
+      }  hover:bg-transparent border border-primaryColor transition duration-300 ${
+        fullWidth ? 'w-full' : 'w-fit'
+      }`}
+    >
+      {title}
+    </button>
+  );
 };
 
 export default PrimaryButton;
