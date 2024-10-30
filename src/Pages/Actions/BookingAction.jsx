ec0256540c7cddc0173fc04882ac3fbd72bfc307
Akm Nahid Bin Wadood <nahidbinwadood@gmail.com>
2024-10-30 14:28:48 +0600

    changing component folder name

--- src/Pages/Actions/BookingAction.jsx
+++ src/Pages/Actions/BookingAction.jsx
@@ -10,13 +10,13 @@ import { TextField } from '@mui/material';
 import { useState } from 'react';
 import { format } from 'date-fns';
 import { cn } from '@/lib/utils';
-import { Button } from '@/components/ui/button';
-import { Calendar } from '@/components/ui/calendar';
+import { Button } from '@/Components/ui/button';
+import { Calendar } from '@/Components/ui/calendar';
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from '@/components/ui/popover';
+} from '@/Components/ui/popover';
 import {
   Select,
   SelectContent,
