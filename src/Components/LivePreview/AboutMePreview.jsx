7b65aeb3f0a6e017d866c6c601e78657dfc899b6
nahidbinwadood <nahidbinwadood@gmail.com>
2025-01-22 18:01:14 +0600

    feat:added about me live preview

--- src/Components/LivePreview/AboutMePreview.jsx
+++ src/Components/LivePreview/AboutMePreview.jsx
@@ -1,10 +1,8 @@
 /* eslint-disable react/prop-types */
-import { Link } from 'react-router-dom';
-import profileImg from '@/assets/images/profile.png';
-import { IoMdMail } from 'react-icons/io';
-import { CallPreviewSvg } from '../SvgContainer/SvgContainer';
 
-const CallPreview = ({ formData }) => {
+import profileImg from '@/assets/images/profile.png';
+
+const AboutMePreview = ({ formData }) => {
   return (
     <div className="w-[450px] font-inter rounded-xl overflow-hidden shadow-xl h-fit bg-gradient-to-l from-[#116DFF] to-[#23C0B6] py-10">
       {/* img */}
@@ -31,38 +29,24 @@ const CallPreview = ({ formData }) => {
           <h4 className=" text-2xl font-semibold text-[#fff]">
             {formData?.name ? formData?.name : 'Enter Your Name'}
           </h4>
-
-          {/* logo */}
-          <div className="w-full flex items-center justify-center py-2">
-            <div className="size-20">
-              <CallPreviewSvg />
-            </div>
-          </div>
         </div>
 
         {/* contact */}
         <div className="space-y-4">
           {/* email */}
           <div className="bg-[#efefef] p-5 rounded-md w-full flex items-center justify-between">
-            <div className="space-y-2 overflow-hidden">
-              <p className="text-primaryColor"> My Number</p>
-              <p className="text-[#555] font-medium w-full max-w-[300px]">
-                {formData?.number ? formData?.number : 'Enter your Number'}
-              </p>
+            <div className="space-y-2 overflow-hidden w-full">
+              <p className="text-primaryColor"> Description:</p>
+              <textarea
+                name=""
+                id=""
+                readOnly
+                placeholder="Enter your description"
+                rows={10}
+                value={formData?.description && formData?.description}
+                className="text-[#555] focus:outline-none font-medium w-full bg-transparent resize-none"
+              ></textarea>
             </div>
-            <div className="flex items-center gap-4 justify-center">
-              <div className="bg-gradient-to-l from-[#116DFF] to-[#23C0B6] size-8 flex items-center justify-center rounded-md">
-                <IoMdMail className="text-white size-4" />
-              </div>
-            </div>
-          </div>
-          <div>
-            <Link
-              to={`tel: ${formData?.number}`}
-              className="bg-[#efefef] text-primaryColor w-full block text-center py-3 rounded-md font-medium"
-            >
-              Contact Me
-            </Link>
           </div>
         </div>
       </div>
@@ -70,4 +54,4 @@ const CallPreview = ({ formData }) => {
   );
 };
 
-export default CallPreview;
+export default AboutMePreview;
--- src/Pages/Actions/AboutmeActions.jsx
+++ src/Pages/Actions/AboutmeActions.jsx
@@ -11,6 +11,7 @@ import toast from 'react-hot-toast';
 import { ImSpinner9 } from 'react-icons/im';
 import { Link, useNavigate } from 'react-router-dom';
 import profile from '@/assets/images/profile.png';
+import AboutMePreview from '@/Components/LivePreview/AboutMePreview';
 
 const AboutmeActions = () => {
   const [profilePhoto, setProfilePhoto] = useState('');
@@ -74,7 +75,7 @@ const AboutmeActions = () => {
   const handleSave = () => {
     console.log(formData);
     setLoading(true);
-    aboutMeMutation.mutate(formData);
+    // aboutMeMutation.mutate(formData);
     // navigate to profile page
   };
 
@@ -125,72 +126,73 @@ const AboutmeActions = () => {
           </div>
         </div>
       </div>
-      <div className="pt-8 pb-8 font-inter bg-[#f3f8fe] min-h-[calc(100vh-104px)]">
-        <div className="container mx-auto">
-          <div className="flex flex-col items-center max-w-[600px] mx-auto mb-20 w-full p-8 rounded-2xl bg-white shadow-lg">
-            {/* image section */}
-            <div>
-              <div className="w-full flex items-center justify-center relative">
-                <div className="size-40 z-10 relative">
-                  <img
-                    className="h-full w-full object-cover rounded-full"
-                    src={
-                      formData?.image
-                        ? URL.createObjectURL(formData.image)
-                        : profilePhoto || profile
-                    }
-                    alt="Profile"
+      <div className="font-inter bg-[#f3f8fe] min-h-[calc(100vh-104px)] flex gap-12 justify-center mx-auto w-full py-20">
+        <div className="flex flex-col items-center w-[600px] p-8 rounded-2xl bg-white shadow-lg">
+          {/* image section */}
+          <div>
+            <div className="w-full flex items-center justify-center relative">
+              <div className="size-40 z-10 relative">
+                <img
+                  className="h-full w-full object-cover rounded-full"
+                  src={
+                    formData?.image
+                      ? URL.createObjectURL(formData.image)
+                      : profilePhoto || profile
+                  }
+                  alt="Profile"
+                />
+                <label
+                  htmlFor="profilePicture"
+                  className="absolute bottom-5 right-0 cursor-pointer"
+                >
+                  <input
+                    onChange={handleProfilePhotoChange}
+                    className="hidden"
+                    type="file"
+                    name="profilePicture"
+                    id="profilePicture"
                   />
-                  <label
-                    htmlFor="profilePicture"
-                    className="absolute bottom-5 right-0 cursor-pointer"
-                  >
-                    <input
-                      onChange={handleProfilePhotoChange}
-                      className="hidden"
-                      type="file"
-                      name="profilePicture"
-                      id="profilePicture"
-                    />
-                    <div className="bg-primaryColor rounded-full size-8 flex items-center justify-center">
-                      <AddImagePlusSvg />
-                    </div>
-                  </label>
-                </div>
-              </div>
-              <div>
-                <h3 className="text-xl font-medium text-center mt-3">
-                  Profile Image
-                </h3>
+                  <div className="bg-primaryColor rounded-full size-8 flex items-center justify-center">
+                    <AddImagePlusSvg />
+                  </div>
+                </label>
               </div>
             </div>
-            <div className="flex gap-4 mt-10 w-full">
-              <div className="flex-shrink-0 flex">
-                <UserSvg />
-              </div>
-              <div className="flex-1 space-y-5">
-                <TextField
-                  label="Enter Your Name"
-                  variant="outlined"
-                  fullWidth
-                  name="name"
-                  value={formData?.name}
-                  onChange={handleChange}
-                />
-                <TextField
-                  multiline
-                  rows={8}
-                  label="Description"
-                  variant="outlined"
-                  fullWidth
-                  name="description"
-                  value={formData?.description}
-                  onChange={handleChange}
-                />
-              </div>
+            <div>
+              <h3 className="text-xl font-medium text-center mt-3">
+                Profile Image
+              </h3>
+            </div>
+          </div>
+          <div className="flex gap-4 mt-10 w-full">
+            <div className="flex-shrink-0 flex">
+              <UserSvg />
+            </div>
+            <div className="flex-1 space-y-5">
+              <TextField
+                label="Enter Your Name"
+                variant="outlined"
+                fullWidth
+                name="name"
+                value={formData?.name}
+                onChange={handleChange}
+              />
+              <TextField
+                multiline
+                rows={8}
+                label="Description"
+                variant="outlined"
+                fullWidth
+                name="description"
+                value={formData?.description}
+                onChange={handleChange}
+              />
             </div>
           </div>
         </div>
+
+        {/* Preview */}
+        <AboutMePreview formData={formData} />
       </div>
     </>
   );
